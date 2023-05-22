be1ea20e85587cbce5ecd72e1f5f0bfeb9aeda82
igorK <i.kolegov78@gmail.com>
2023-05-22 20:28:18 +0300

    First commit

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,7 +4,7 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 3
     assert arrs.get([], 0, "test") == "test"
-
+git
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
